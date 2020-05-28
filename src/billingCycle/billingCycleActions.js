0f6e62e4ed813c3077bae10d02f58b1f584db4d7
Ednardo Nobre <ednardo.nobre@objective.com.br>
2020-05-28 10:22:06 -0300

    Corrigindo URLs apontando para localhost

--- src/billingCycle/billingCycleActions.js
+++ src/billingCycle/billingCycleActions.js
@@ -2,13 +2,14 @@ import axios from 'axios'
 import { toastr } from 'react-redux-toastr'
 import { reset as resetForm, initialize } from 'redux-form'
 
+import consts from '../consts'
 import { showTabs, selectTab } from '../common/tab/tabActions'
 
-const BASE_URL = 'http://localhost:3003/api'
 const INITIAL_VALUES = { credits: [{}], debts: [{}]}
 
 export function getList() {
-    const request = axios.get(`${BASE_URL}/billingCycles`)
+    
+    const request = axios.get(`${consts.API_URL}/billingCycles`)
     return {
         type: 'BILLING_CYCLES_FETCHED',
         payload: request
@@ -30,7 +31,7 @@ export function remove(values) {
 function submit(values, method) {
     return dispatch => {
         const id = values._id ? values._id : ''
-        axios[method](`${BASE_URL}/billingCycles/${id}`, values)
+        axios[method](`${consts.API_URL}/billingCycles/${id}`, values)
             .then(resp => {
                 toastr.success('Sucesso', 'Operação realizada com sucesso.')
                 dispatch(init())
